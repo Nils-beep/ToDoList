df9e82bd3db8a0577954b634250af4f4b257745d
Nils-beep <nils.zacher05@gmail.com>
2026-09-08 22:33:28 +0200

    fixed input handeling std::cin<<x is now std::readline()

--- main.cpp
+++ main.cpp
@@ -13,27 +13,37 @@ std::list<Task> tasks;
 
 int runner(){
     std::cout << "0: list Tasks (" << tasks.size() << ") \n";
-    std::cout << "1: create new Task \n" << "---------------------------------------------- \n";
+    std::cout << "1: create new Task \n";
+    std::cout << "leave blank to stop \n";
 
     std::string text;
-    std::cin >> text;
+    std::getline(std::cin, text);
+
+    // stop
+    if (text == "")
+        return 1;
+
+    // list all
     if (text == "0"){
         for (Task i : tasks)
             std::cout<< i.name << "\n";
     }
-
+    // new task
     if (text == "1"){
         std::string newTaskname;
         std::getline(std::cin, newTaskname);
         if (newTaskname != "")
             tasks.push_back({newTaskname});
     }
+
     return 0;
 }
 
 int main(){
     std::cout << "Task programm started BUH \n" ;
     tasks.push_back({"woah"});
-    while (true)
-        runner();
+    int result = -1;
+    while (result != 1)
+       result = runner();
+    return 0;
 }
