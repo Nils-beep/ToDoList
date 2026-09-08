a33da1e3eebc6d8966a773c6423ade4ce3c0cb02
Nils-beep <nils.zacher05@gmail.com>
2026-09-08 23:58:05 +0200

    task removal logic implemented

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include <istream>
 #include <string>
 #include <list>
+#include <fstream>
+#include <iterator>
 
 
 struct Task{
@@ -14,6 +16,7 @@ std::list<Task> tasks;
 int runner(){
     std::cout << "0: list Tasks (" << tasks.size() << ") \n";
     std::cout << "1: create new Task \n";
+    std::cout << "2: finish task \n";
     std::cout << "leave blank to stop \n";
 
     std::string text;
@@ -35,15 +38,40 @@ int runner(){
         if (newTaskname != "")
             tasks.push_back({newTaskname});
     }
-
+    if (text == "2"){
+        std::string number;
+        std::getline(std::cin, number);
+        if (number != ""){
+            int intNumber = std::stoi(number);
+            auto it = std::next(tasks.begin(), intNumber);
+            tasks.erase(it);
+        }
+    }
     return 0;
 }
 
+void readFile(){
+    std::ifstream myfile;
+    myfile.open("tasklist.txt");
+    std::string line;
+    while (getline(myfile, line))
+        tasks.push_back({line});
+    myfile.close();
+}
+
+void writeFile(){
+    std::ofstream myfile;
+    myfile.open("tasklist.txt");
+    for (Task task : tasks)
+        myfile << task.name << "\n";
+    myfile.close();
+}
+
 int main(){
-    std::cout << "Task programm started BUH \n" ;
-    tasks.push_back({"woah"});
+    readFile();
     int result = -1;
     while (result != 1)
        result = runner();
+    writeFile();
     return 0;
 }
